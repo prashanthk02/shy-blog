88df2973120f1c5450c5608c4e04ba04740de512
prashanthk02 <prashanth.k369@gmail.com>
2022-11-29 13:27:21 -0500

    format document

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import CreatePost from './pages/CreatePost';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/createpost' element={<CreatePost />} />
-        <Route path='/login' element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
