fa9f1cec7f9043373864c12fa6685da61d67afe1
prashanthk02 <prashanth.k369@gmail.com>
2022-11-29 13:21:29 -0500

    Created routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/createpost' element={<CreatePost />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
