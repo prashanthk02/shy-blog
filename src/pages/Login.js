94a207d04773739d82e64f6f022d696074a04ba7
prashanthk02 <prashanth.k369@gmail.com>
2022-12-01 01:56:50 -0500

    create login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { auth, provider } from "../firebase-config";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <form>
+        <label>Email: </label>
+        <input type="email" />
+        <br />
+        <label>Password: </label>
+        <input type="password" />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
