ca02a9e2ba9f8ad7b078e9f127e6047b1dd44e46
prashanthk02 <prashanth.k369@gmail.com>
2022-12-01 02:32:42 -0500

    add login function

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth, provider } from "../firebase-config";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        alert("login success");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode);
+      });
+  };
+
   return (
     <div>
       <form>
         <label>Email: </label>
-        <input type="email" />
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
         <br />
         <label>Password: </label>
-        <input type="password" />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
         <br />
-        <button type="submit">Login</button>
+        <button type="submit" onClick={login}>
+          Login
+        </button>
       </form>
     </div>
   );
