fc36b71808da1707949b1da09ea4fec23dbbccaf
prashanthk02 <prashanth.k369@gmail.com>
2022-12-07 22:03:56 -0500

    fixed login issue

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,39 +1,38 @@
 import React, { useState } from "react";
-import { auth, provider } from "../firebase-config";
+import { auth } from "../firebase-config";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const login = () => {
+  const signIn = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
-        alert("login success");
+        console.log(user);
+        alert("signIn success");
         // ...
       })
       .catch((error) => {
-        const errorCode = error.code;
+        //const errorCode = error.code;
         const errorMessage = error.message;
-        alert(errorCode);
+        alert(errorMessage);
       });
   };
 
   return (
     <div>
-      <form>
+      <div>
         <label>Email: </label>
-        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <input type={"email"} onChange={(e) => setEmail(e.target.value)} />
         <br />
         <label>Password: </label>
-        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <input type={"password"} onChange={(e) => setPassword(e.target.value)} />
         <br />
-        <button type="submit" onClick={login}>
-          Login
-        </button>
-      </form>
+        <button onClick={signIn}>Login</button>
+      </div>
     </div>
   );
 }
