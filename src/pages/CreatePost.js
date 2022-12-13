5e30b1dd477cdd42de9ee7b967d2d2731e749e90
prashanthk02 <prashanth.k369@gmail.com>
2022-12-12 22:34:55 -0500

    add post input fields

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function CreatePost() {
-  return <div>Create Post</div>;
+  return (
+    <div className="cpPage">
+      <div className="cpContainer">
+        <div className="title">
+          <label>Title: </label>
+          <input placeholder="Title..." />
+        </div>
+        <div className="post">
+          <label>Post: </label>
+          <textarea placeholder="Post..." />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreatePost;
