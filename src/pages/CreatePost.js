553e7af571c16a464c1ce54bed2d7646a333d885
prashanthk02 <prashanth.k369@gmail.com>
2022-12-12 22:55:38 -0500

    add state

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
+
   return (
     <div className="cpPage">
       <div className="cpContainer">
         <h1>Create a Post</h1>
         <div className="title">
           <label>Title: </label>
-          <input placeholder="Title..." />
+          <input
+            placeholder="Title..."
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
         </div>
         <div className="post">
           <label>Post: </label>
-          <textarea placeholder="Post..." />
+          <textarea
+            placeholder="Post..."
+            onChange={(e) => {
+              setPostText(e.target.value);
+            }}
+          />
         </div>
         <button>Submit Post</button>
       </div>
