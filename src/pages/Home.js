b97462ca0a21b08fd17bef641a9fbc8400ce1f7b
prashanthk02 <prashanth.k369@gmail.com>
2022-12-14 19:17:28 -0500

    add getdoc function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firebase-config";
 
 function Home() {
-  return <div>Home</div>;
+  const [postList, setPostList] = useState([]);
+  const postCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(postList)
+    };
+
+    getPosts();
+  }, []);
+
+  return (<div className="homePage">
+    {postList.map((post) =>{
+      return (
+        <div className="post">{post.title}</div>
+      )
+    })}
+  </div>);
 }
 
 export default Home;
