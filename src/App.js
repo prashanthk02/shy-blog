8a8c5aadc1fc56c0cc9a84e44ab832a74daac0bd
prashanthk02 <prashanth.k369@gmail.com>
2022-12-14 19:39:44 -0500

    add style folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
