f4e5e58c3a752e22b1cc6846ea83f704b3d3b06c
prashanthk02 <prashanth.k369@gmail.com>
2023-01-20 13:03:29 -0500

    format code

--- src/components/FilterPost.js
+++ src/components/FilterPost.js
@@ -4,10 +4,34 @@ import "../styles/filter.scss";
 export default function FilterPost({ filterValueSelected }) {
   return (
     <div className="filter">
-      <button name="category" value="All" onClick={(e)=>filterValueSelected(e.target.value)}>All</button>
-      <button name="category" value="A" onClick={(e)=>filterValueSelected(e.target.value)}>A</button>
-      <button name="category" value="B" onClick={(e)=>filterValueSelected(e.target.value)}>B</button>
-      <button name="category" value="C" onClick={(e)=>filterValueSelected(e.target.value)}>C</button>
+      <button
+        name="category"
+        value="All"
+        onClick={(e) => filterValueSelected(e.target.value)}
+      >
+        All
+      </button>
+      <button
+        name="category"
+        value="A"
+        onClick={(e) => filterValueSelected(e.target.value)}
+      >
+        A
+      </button>
+      <button
+        name="category"
+        value="B"
+        onClick={(e) => filterValueSelected(e.target.value)}
+      >
+        B
+      </button>
+      <button
+        name="category"
+        value="C"
+        onClick={(e) => filterValueSelected(e.target.value)}
+      >
+        C
+      </button>
     </div>
   );
 }
