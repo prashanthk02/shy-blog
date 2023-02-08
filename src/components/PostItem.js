5c35aa24af2e0f8539b79cec8e71e90d247f89b6
prashanthk02 <prashanth.k369@gmail.com>
2023-02-08 00:31:13 -0500

    add delete option to post card

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,34 +1,14 @@
 import React from "react";
-import { deleteDoc, doc } from "firebase/firestore";
-import { db } from "../firebase-config";
 import "../styles/posts.scss";
 
 export default function PostItem(props) {
-  const currentUserState = props.currentUserState
-  const isAuth = props.isAuth;
   const id = props.id;
   const date = new Date(props.date.seconds * 1000 + props.date.nanoseconds/1000000);
-
-  const deletePost = async (id) => {
-    const postDoc = doc(db, "posts", id);
-    await deleteDoc(postDoc);
-    window.location.reload();
-  };
 
   return (
     <div className="post" key={id}>
       <div className="postDate">
         <h6>{date.toDateString()}</h6>
-        {isAuth && props.email === currentUserState.email && (
-          <button
-            className="postDelete"
-            onClick={() => {
-              deletePost(id);
-            }}
-          >
-            X
-          </button>
-        )}
       </div>
       <h6>{props.category}</h6>
       <div className="title">
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { getDocs, collection } from "firebase/firestore";
-import { db } from "../firebase-config";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { auth, db } from "../firebase-config";
 import { Link } from "react-router-dom";
 
-import PostItem from "./PostItem";
 import "../styles/posts.scss";
 
 function Posts({ isAuth, filterValue }) {
@@ -22,6 +21,12 @@ function Posts({ isAuth, filterValue }) {
     }
   });
 
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+    window.location.reload();
+  };
+
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(postCollectionRef);
@@ -37,7 +42,23 @@ function Posts({ isAuth, filterValue }) {
       <div className="postCards">
         {filteredPosts.map((post) => {
           return (
-            <div className="postCard" key={post.id} onClick={() => localStorage.setItem(post.id, JSON.stringify(post))}>
+            <div
+              className="postCard"
+              key={post.id}
+              onClick={() =>
+                localStorage.setItem(post.id, JSON.stringify(post))
+              }
+            >
+              {isAuth && post.email === auth.currentUser.email && (
+                <button
+                  className="postDelete"
+                  onClick={() => {
+                    deletePost(post.id);
+                  }}
+                >
+                  X
+                </button>
+              )}
               <Link to={`/post/${post.id}`}>
                 <img src={post.url} alt={post.title} width="300" height="200" />
                 <span>{post.title}</span>
