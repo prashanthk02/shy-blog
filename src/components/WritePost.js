ad683ba432cfc282c18f1fa360fb5701ae7d8364
prashanthk02 <prashanth.k369@gmail.com>
2023-02-22 13:34:00 -0500

    chnage write post page style

--- src/components/WritePost.js
+++ src/components/WritePost.js
@@ -4,7 +4,7 @@ import { auth, db } from "../firebase-config";
 import { useNavigate } from "react-router-dom";
 
 import MyEditor from "./MyEditor";
-import "../styles/writePost.scss"
+import "../styles/writePost.scss";
 
 function WritePost({ isAuth }) {
   const [category, setCategory] = useState("Lifestyle");
@@ -19,7 +19,15 @@ function WritePost({ isAuth }) {
   const writePost = async () => {
     const date = Timestamp.now();
     const email = auth.currentUser.email;
-    await addDoc(postCollectionRef, { category, title, postText, author, email, date, url });
+    await addDoc(postCollectionRef, {
+      category,
+      title,
+      postText,
+      author,
+      email,
+      date,
+      url,
+    });
     navigate("/");
   };
 
@@ -33,13 +41,13 @@ function WritePost({ isAuth }) {
     <div className="cpPage">
       <h1>Create a Post</h1>
       <div className="cpContainer">
-
-       <div className="categoryText">
+        <div className="postDetails">
+        <div className="categoryText">
           <label>Category: </label>
-          <select onChange={(e) => {
+          <select
+            onChange={(e) => {
               setCategory(e.target.value);
-              }
-            }
+            }}
           >
             <option value="Lifestyle">Life Style</option>
             <option value="Travel">Travel</option>
@@ -57,25 +65,6 @@ function WritePost({ isAuth }) {
             }}
           />
         </div>
-        {/* <div className="postText">
-          <label>Post: </label>
-          <textarea
-            placeholder="Post..."
-            type="text"
-            onChange={(e) => {
-              setPostText(e.target.value);
-            }}
-          />
-        </div> */}
-        <div className="authorText">
-          <label>Author: </label>
-          <input
-            placeholder="Author..."
-            onChange={(e) => {
-              setAuthor(e.target.value);
-            }}
-          />
-        </div>
 
         <div className="imgUrl">
           <label>Image URL: </label>
@@ -86,8 +75,19 @@ function WritePost({ isAuth }) {
             }}
           />
         </div>
-        
-        <div className="editor" >
+
+        <div className="authorText">
+          <label>Author: </label>
+          <input
+            placeholder="Author..."
+            onChange={(e) => {
+              setAuthor(e.target.value);
+            }}
+          />
+        </div>
+        </div>
+
+        <div className="editor">
           <MyEditor setPostText={setPostText} />
         </div>
 
