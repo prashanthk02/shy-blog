952aaf33f5a76aa251a79863a99be64bfa9352bd
prashanthk02 <prashanth.k369@gmail.com>
2023-03-08 22:07:49 -0500

    add style to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import React from "react";
+import "../styles/about.scss";
 
 export default function about() {
-  return <div>Nothing yet here, Please come back later.</div>;
+  return <div className="about">Nothing here yet, Please come back later.</div>;
 }
