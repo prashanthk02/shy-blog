bbe27000b74e2d0b0e72b50b0feca3ded4d7f9c3
prashanthk02 <prashanth.k369@gmail.com>
2023-03-16 16:02:36 -0400

    display loader in about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/about.scss";
+import Loader from "../components/Loader";
 
 export default function about() {
-  return <div className="about">Nothing here yet, Please come back later.</div>;
+  return <Loader />
+  // return <div className="about">Nothing here yet, Please come back later.</div>;
 }
